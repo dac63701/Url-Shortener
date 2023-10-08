1a87f1a0fac35e7c3ba7f652d5a97cbe16a5414e
dac63701 <66576133+dac63701@users.noreply.github.com>
2023-10-08 16:44:32 -0400

    yes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-import logo from './logo.svg';
 import './App.css';
 
 import { hash } from './hash'
@@ -30,20 +29,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      This is a Test
     </div>
   );
 }
